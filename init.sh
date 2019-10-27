28002d437ed57b42be3a5cb205bbfbb756367141
Alexis Kinsella <alexis.kinsella@gmail.com>
2019-10-27 23:41:14 +0100

    Added support for geopandas analysis

--- init.sh
+++ init.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+sudo apt install python3-rtree
+
 # Install gmaps for jupyter notebooks
 # With pip: https://jupyter-gmaps.readthedocs.io/en/latest/install.html#installing-jupyter-gmaps-with-pip
 pip install jupyterlab
-jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
 pip install -r requirements.txt
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py --sys-prefix gmaps
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+jupyter nbextension install --py --sys-prefix keplergl
+jupyter nbextension enable --py --sys-prefix keplergl
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+jupyter labextension install @jupyter-widgets/jupyterlab-manager keplergl-jupyter
+
 jupyter lab build
 echo "Jupyter version: $(jupyter notebook --version)"
