82fa94cb86ea63732cfdac60628c55e3a1bf4def
nlaille <nlaille@xebia.fr>
2019-11-05 11:17:36 +0100

    changes jupyterhub values and jupyter config

--- xebicon-notebook/jupyter_notebook_config.py
+++ xebicon-notebook/jupyter_notebook_config.py
@@ -63,13 +63,13 @@ from bookstore import BookstoreContentsArchiver
 c.NotebookApp.contents_manager_class = BookstoreContentsArchiver
 
 # All Bookstore settings are centralized on one config object so you don't have to configure it for each class
-c.BookstoreSettings.workspace_prefix = "/workspace/kylek/notebooks"
-c.BookstoreSettings.published_prefix = "/published/kylek/notebooks"
+c.BookstoreSettings.workspace_prefix = "/workspace/notebooks/" + os.environ["JUPYTERHUB_USER"]
+c.BookstoreSettings.published_prefix = "/published/notebooks/" + os.environ["JUPYTERHUB_USER"]
 
-c.BookstoreSettings.s3_bucket = "bookstore"
+c.BookstoreSettings.s3_bucket = os.getenv("BOOKSTORE_S3_BUCKET", "bookstore")
 
 # Note: if bookstore is used from an EC2 instance with the right IAM role, you don't
 # have to specify these
-c.BookstoreSettings.s3_access_key_id = "minio"
-c.BookstoreSettings.s3_secret_access_key = "minio123"
-c.BookstoreSettings.s3_endpoint_url = "http://minio:9000/"
+c.BookstoreSettings.s3_access_key_id = os.getenv("BOOKSTORE_S3_ACCESS_KEY_ID", "minio")
+c.BookstoreSettings.s3_secret_access_key = os.getenv("BOOKSTORE_S3_ACCESS_KEY", "minio123")
+c.BookstoreSettings.s3_endpoint_url = os.getenv("BOOKSTORE_S3_ENDPOINT_URL", "http://minio:9000/")
