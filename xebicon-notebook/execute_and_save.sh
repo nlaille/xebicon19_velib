d10ce37241674b1675511fd5392fcf73d8091234
nlaille <nlaille@xebia.fr>
2019-11-07 18:41:18 +0100

    fix notebook and script

--- xebicon-notebook/execute_and_save.sh
+++ xebicon-notebook/execute_and_save.sh
@@ -12,5 +12,5 @@ HOUR=$4
 
 echo "Executing script for $1-$2-$3 - $4h..."
 
-papermill velib_stations_usage.ipynb /tmp/output.ipynb -p year=$YEAR -p month=$MONTH -p day=$DAY -p hour=$HOUR
-aws s3 cp /tmp/output.ipynb s3://velib-usage/$YEAR-$MONTH-$DAY-$HOUR.ipynb
+papermill velib_stations_usage.ipynb /tmp/output.ipynb -p year $YEAR -p month $MONTH -p day $DAY -p hour $HOUR
+aws s3 cp --endpoint-url $AWS_S3_ENDPOINT /tmp/output.ipynb s3://velib-usage/$YEAR-$MONTH-$DAY-$HOUR.ipynb
